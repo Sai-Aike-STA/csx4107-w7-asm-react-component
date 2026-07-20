bc53edc31e06197cb1e9599ba5492f112030d8d9
sasta-kro <sai.aike.shwe.tun.aung@gmail.com>
2026-07-20 12:12:37 +0700

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 import Greetings from './components/Greetings.jsx'
 import MyHobbies from './components/MyHobbies.jsx'
 import Profile from './components/Profile.jsx'
+import About from "./components/About.jsx";
 
 function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Greetings />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/my-hobbies" element={<MyHobbies />} />
-      </Routes>
-    </HashRouter>
-  )
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Greetings/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/my-hobbies" element={<MyHobbies/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+        </HashRouter>
+    )
 }
 
 export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import ContentCard from "./ContentCard.jsx";
+
+function About() {
+    return (
+        <ContentCard
+            title="About"
+            content={"This is an about page"}
+
+        />
+    )
+}
+
+export default About
